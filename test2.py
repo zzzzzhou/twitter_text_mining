8c28e9bee9a5ebcb7128c6b39c5de9ffb832fd14
zzzzzhou <913558352@qq.com>
2022-02-24 22:18:52 +0000

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,18 @@
+import tweepy
+
+bear_token = 'AAAAAAAAAAAAAAAAAAAAAGPBWgEAAAAAedocddgJGmU%2FY5VlTRIPQSkME6M%3DV05hdBJfmWVwWWZxSEXFqrSCbRcnLwSAf57vpz337OYgxXKKEx'
+client = tweepy.Client(bear_token)
+tweets_fields_list = ['created_at', 'id', 'text']
+
+# for response in tweepy.Paginator(client.search_recent_tweets, query="Trump", tweet_fields=tweets_fields_list,
+#                                  max_results=10, limit=2):
+
+for response in tweepy.Paginator(client.search_recent_tweets, query="Trump -is:retweet",
+                                 tweet_fields=tweets_fields_list,
+                                 max_results=10, limit=2):
+
+    # print(response.includes)
+    for tweet in response.data:
+        print(tweet.id, tweet.created_at, tweet.text)
+
+print('the type of tweet is', type(tweet))
