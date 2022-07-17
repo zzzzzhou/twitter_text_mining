38a612824da3af677b1853e725f3e28347df46ed
ZHEDI ZHOU <913558352@qq.com>
2022-07-17 17:03:42 +0800

    Update upload_2_S3.py

--- twitter api crawl/upload_2_S3.py
+++ twitter api crawl/upload_2_S3.py
@@ -1,6 +1,6 @@
 import boto3
 import os
-s3 = boto3.client('s3', aws_access_key_id='AKIAQKRHIHI6MNEUI4OZ', aws_secret_access_key='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')
+s3 = boto3.client('s3', aws_access_key_id='', aws_secret_access_key='')
 s3.upload_file(
     Filename="new_cosmetics_brand.csv",
     Bucket="cosmetics-tweets",
